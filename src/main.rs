f8fe5dd55d1d7b0fdb1cc54e86cdabfc840edd5a
Ciffelia <mc.prince.0203@gmail.com>
2024-03-15 17:11:10 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::time::Duration;
 
-use aws_sdk_s3::{presigning::PresigningConfig, primitives::ByteStream, types::ChecksumAlgorithm};
+use aws_sdk_s3::{presigning::PresigningConfig, types::ChecksumAlgorithm};
 
 #[tokio::main]
 async fn main() {
     let client = aws_sdk_s3::Client::new(&aws_config::load_from_env().await);
 
+    // checksum for b"abc"
     let checksum = "ungWv48Bz+pBQUDeXa4iI7ADYaOWF3qctBD/YfIAFa0=";
 
     let presigned_request = client
@@ -14,7 +15,6 @@ async fn main() {
         .key("foo")
         .checksum_algorithm(ChecksumAlgorithm::Sha256)
         .checksum_sha256(checksum)
-        .body(ByteStream::from_static(b"abc"))
         .presigned(PresigningConfig::expires_in(Duration::from_secs(360)).unwrap())
         .await
         .unwrap();
