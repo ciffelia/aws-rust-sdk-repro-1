ca0e6c0c0db4c94c64e3f0dde1f37164fddcdb37
Ciffelia <mc.prince.0203@gmail.com>
2024-03-15 16:49:04 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
 use std::time::Duration;
 
-use aws_sdk_s3::{
-    presigning::{PresignedRequest, PresigningConfig},
-    primitives::ByteStream,
-    types::ChecksumAlgorithm,
-};
+use aws_sdk_s3::{presigning::PresigningConfig, primitives::ByteStream, types::ChecksumAlgorithm};
 
 #[tokio::main]
 async fn main() {
     let client = aws_sdk_s3::Client::new(&aws_config::load_from_env().await);
 
-    let req1 =
-        create_presigned_request(&client, "ungWv48Bz+pBQUDeXa4iI7ADYaOWF3qctBD/YfIAFa0=").await;
-    let req2 =
-        create_presigned_request(&client, "Ngi8oeROpsTSaOttsCJgJpiSwLQrhrvx53pvoWw8koI=").await;
+    let checksum = "ungWv48Bz+pBQUDeXa4iI7ADYaOWF3qctBD/YfIAFa0=";
 
-    assert_ne!(
-        req1.headers().collect::<Vec<_>>(),
-        req2.headers().collect::<Vec<_>>()
-    );
-}
-
-async fn create_presigned_request(client: &aws_sdk_s3::Client, checksum: &str) -> PresignedRequest {
-    client
+    let presigned_request = client
         .put_object()
         .bucket("example-bucket")
         .key("foo")
@@ -31,5 +17,11 @@ async fn create_presigned_request(client: &aws_sdk_s3::Client, checksum: &str) -
         .body(ByteStream::from_static(b""))
         .presigned(PresigningConfig::expires_in(Duration::from_secs(360)).unwrap())
         .await
-        .unwrap()
+        .unwrap();
+
+    dbg!(presigned_request.headers().collect::<Vec<_>>());
+
+    assert!(presigned_request
+        .headers()
+        .any(|(k, v)| { k == "x-amz-content-sha256" && v == checksum }));
 }
